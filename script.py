69a2ee7c8cec23ab6d39831f7ef45530c9cc588b
louisspaghetti <46613855+louisspaghetti@users.noreply.github.com>
2019-12-20 23:12:55 -0500

    Update script.py

--- script.py
+++ script.py
@@ -1,12 +1,15 @@
 import random
 
 running = True
-level = 1
+level = 0
+moves_remaining = 0
+starting_value = 1
+operator = 2
 operation_list = ["addition", "subtraction", "multiplication", "division"]
 
 #recursive function that applies random operations to a base number to scramble it
-def apply_operations(base, operator = 2, iterations = 1):
-    operation = operation_list[random.randint(0,3)]
+def apply_operations(base, operator = 2, iterations = 1, index = random.randint(0,3)):
+    operation = operation_list[index]
     if iterations == 0:
         return base
     if operation == "addition":
@@ -19,3 +22,15 @@ def apply_operations(base, operator = 2, iterations = 1):
         return apply_operations(base / operator, operator, iterations - 1)
     return
 
+
+while running:
+    if moves_remaining <= 0:
+        level += 1
+        current_value = apply_operations(starting_value, operator, level, random.randint(0,3))
+        moves_remaining = level
+    print(level)
+    print(moves_remaining)
+    print(current_value)
+    print("Enter the operation")
+    current_value = apply_operations(current_value, operator, 1, operation_list.index(input()))
+    moves_remaining -= 1
